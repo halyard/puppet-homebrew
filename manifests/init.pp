08039af956dfbf40d2630e4b9ef7437e01a1e79d
Les Aker <me@lesaker.org>
2017-01-08 20:33:15 -0800

    add simple repo declaration

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,12 @@
 
 class homebrew (
   $path = '/usr/local',
-  $user = $facts['id']
+  $user = $facts['id'],
+  $repo = 'git://github.com/homebrew/brew'
 ) {
+  vcsrepo { $path:
+    ensure   => present,
+    provider => git,
+    source   => $repo,
+  }
 }
