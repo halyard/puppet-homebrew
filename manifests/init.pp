9b92644e72603e4d2bf893e386020c387f686d69
Les Aker <me@lesaker.org>
2017-01-08 22:05:47 -0800

    set owner and group

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,15 @@
 
 class homebrew (
   $path = '/usr/local',
-  $user = $facts['id'],
+  $owner = $facts['id'],
+  $group = $facts['gid'],
   $repo = 'git://github.com/homebrew/brew'
 ) {
   vcsrepo { $path:
     ensure   => present,
     provider => git,
     source   => $repo,
+    owner    => $owner,
+    group    => $group
   }
 }
