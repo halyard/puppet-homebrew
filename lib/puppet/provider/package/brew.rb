b40cb6251327a38e51637c3c1cb53992dcab15bd
Les Aker <me@lesaker.org>
2017-01-13 18:18:15 -0500

    first successful run of brew install

--- lib/puppet/provider/package/brew.rb
+++ lib/puppet/provider/package/brew.rb
@@ -1,9 +1,66 @@
 require 'puppet/provider/package'
+require 'etc'
 
 Puppet::Type.type(:package).provide :brew, :parent => Puppet::Provider::Package do
   desc "Homebrew support for Mac packages"
 
-  has_feature :installable, :upgradable, :uninstallable, :versionable, :install_options, :uninstall_options
+  mk_resource_methods
 
-  commands :brewcmd => "#{Puppet[:'homebrew::path']}/bin/brew"
+  has_feature :installable, :uninstallable
+
+  # TODO: support alternate brew paths
+  commands :brewcmd => "/opt/brew/bin/brew"
+
+  def self.package_list
+    command = [command(:brewcmd), 'list', '--full-name']
+    @package_list ||= execute(command).split.map do |pkg_name|
+      {
+        :name     => pkg_name,
+        :ensure   => :present,
+        :provider => :brew
+      }
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def uninstall
+    execute([command(:brewcmd), 'uninstall', resource[:name]])
+  end
+
+  def install
+    execute([command(:brewcmd), 'install', resource[:name]])
+  end
+
+  def update
+    install(false)
+  end
+
+  def query
+    self.class.package_list.find { |x| x[:name] == resource[:name].downcase }
+  end
+
+  def self.instances
+    package_list.map { |hash| new(hash) }
+  end
+
+  def execute(cmd)
+    self.class.execute(cmd)
+  end
+
+  def self.execute(cmd)
+    stat = File.stat(command(:brewcmd))
+    home = Etc.getpwuid(stat.uid).dir
+    Puppet.debug(cmd.to_s)
+    super(
+      cmd,
+      :uid => stat.uid,
+      :gid => stat.gid,
+      :failonfail => true,
+      :combine => true,
+      :custom_environment => { 'HOME' => home }
+    )
+  end
 end
