b1e98117b95fd9633b8b2c5f8ae74a673233a450
Les Aker <me@lesaker.org>
2017-01-14 11:39:24 -0500

    first pass of taps

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,9 @@ class homebrew (
   $path = '/usr/local',
   $owner = $facts['id'],
   $group = $facts['gid'],
-  $repo = 'https://github.com/homebrew/brew'
+  $repo = 'https://github.com/homebrew/brew',
+  $taps = [],
+  $formulae = []
 ) {
   vcsrepo { $path:
     ensure   => present,
@@ -15,4 +17,19 @@ class homebrew (
     owner    => $owner,
     group    => $group
   }
+
+  $taps.each |String[1] $tap| {
+    package { $tap:
+      provider => tap
+    }
+  }
+
+  $formulae.each |String[1] $formula| {
+    package { $formula:
+      provider => brew
+    }
+  }
+
+  Package <| provider == tap |> -> Package <| provider == brew |>
+  Package <| provider == tap |> -> Package <| provider == cask |>
 }
